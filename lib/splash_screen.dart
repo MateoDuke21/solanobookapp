d7519e6160788ce60e392ad6dd15831553bcd9f0
Jhon Matthew Evangelista <143513397+MateoDuke21@users.noreply.github.com>
2024-07-17 17:07:35 +0800

    change splashscreen and pubspec

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'authentication_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatefulWidget {
+  // ignore: use_super_parameters
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _SplashScreenState createState() => _SplashScreenState();
 }
 
@@ -18,6 +21,7 @@ class _SplashScreenState extends State<SplashScreen> {
   _navigateToHome() async {
     await Future.delayed(const Duration(seconds: 3), () {});
     Navigator.pushReplacement(
+      // ignore: use_build_context_synchronously
       context,
       MaterialPageRoute(builder: (context) => const AuthenticationScreen()),
     );
@@ -26,10 +30,34 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(
-          color: Colors.lightGreen[700],
-        ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/images/Splashscreen-bg.png',
+            fit: BoxFit.fill,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Color(0xffe8c94a)),
+                  backgroundColor: Color(0xff1c4440),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Making your stay genuinely delightful',
+                  style: GoogleFonts.poppins(
+                    fontSize: 18,
+                    color: Colors.amber[400],
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
